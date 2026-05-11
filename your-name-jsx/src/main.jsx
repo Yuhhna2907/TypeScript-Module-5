c5dd6aa09e7788ffaee0caee05ec112b4439f8b7
Lương Anh Huy <luonganhhuy2004@gmail.com>
2026-05-11 16:11:08 +0700

    bt

---
+++ your-name-jsx/src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const name = "Your name";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<h1 style={{ textAlign: "center" }}>{name}</h1>);
